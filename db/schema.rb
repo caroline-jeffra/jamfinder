720bffd5d8973f04bd08515b38e5b8f84a03d306
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-08-28 15:32:56 +0200

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_28_125213) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_130811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jams", force: :cascade do |t|
+    t.date "date"
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.boolean "did_happen", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
