a9e1e079a0a1a575ba1a4dc6f7f3d65d378a207f
Thiago Souza <thlucioxi@gmail.com>
2023-08-28 16:16:24 +0200

    finish instruments

--- db/migrate/20230828141315_create_user_instruments.rb
+++ db/migrate/20230828141315_create_user_instruments.rb
@@ -1,8 +1,8 @@
 class CreateUserInstruments < ActiveRecord::Migration[7.0]
   def change
     create_table :user_instruments do |t|
-      t.references  :user, foreign_key: true
-      t.references  :instrument, foreign_key: true
+      t.references  :user, null: false, foreign_key: true
+      t.references  :instrument, null: false, foreign_key: true
 
       t.timestamps
     end
