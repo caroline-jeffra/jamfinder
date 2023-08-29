1daf7309cc0ae53d0759ccb4339daafc44ee542e
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-08-29 16:50:03 +0200

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :profiles, only: %i[index show]
+  resources :chatrooms, only: %i[index show], path: :chats, as: :chats do
+    resource :messages, only: %i[create]
+  end
+  post :jams, to: 'jams#create'
+  get '/dashboard', to: 'dashboards#show'
 end
