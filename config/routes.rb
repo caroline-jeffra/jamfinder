bee9370619aee3877447e2e0aafce2d515e7ae3e
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-08-29 17:31:37 +0200

    corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :profiles, only: %i[index show]
-  resources :chatrooms, only: %i[index show], path: :chats, as: :chats do
-    resource :messages, only: %i[create]
+  resources :chatrooms, only: :index, path: :chats, as: :chats do
+    resources :messages, only: :create
   end
-  post :jams, to: 'jams#create'
-  get '/dashboard', to: 'dashboards#show'
+  resources :jams, only: :create
+  resources :genres, only: :create
+  resources :instruments, only: :create
+  
+  get '/dashboard', to: 'pages#dashboard'
 end
