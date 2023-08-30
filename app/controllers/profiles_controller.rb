354e61346cb97be90023d4fce3e134390c5cb690
Thiago Souza <thlucioxi@gmail.com>
2023-08-30 16:42:08 +0200

    Implement mapbox

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,12 @@ class ProfilesController < ApplicationController
   def index
     @profiles = User.where.not(id: current_user)
     # TODO: account for search filters
+    @markers = @profiles.geocoded.map do |user|
+      {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
   end
 
   def show
