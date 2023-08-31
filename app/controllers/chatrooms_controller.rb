f0f4b9cd594712a80cc6ac9896fd38fbd2a37484
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-08-31 14:16:58 +0200

    chats participants column, new chat creation logic, user chats logic

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,9 @@
 class ChatroomsController < ApplicationController
   def index
     # TODO: based on current_user, find all Chatroom IDs the user is attached to through the user_chatrooms table and display them
+    @participants = [current_user.id, params[:recipient].to_i].sort
+    Chatroom.create!(participant_ids: @participants) unless Chatroom.where(participant_ids: @participants).any?
+    @chatrooms = Chatroom.where("? = ANY (participant_ids)", current_user.id)
     # TODO: Within the index, the user can click on a shatroom and display its contents.
   end
 end
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,4 +1,13 @@
 class Chatroom < ApplicationRecord
   has_many :messages
   has_many :user_chatrooms
+  before_validation :sort_participant_ids
+
+  validates_uniqueness_of :participant_ids
+
+  private
+
+  def sort_participant_ids
+    self.participant_ids = participant_ids.sort
+  end
 end
