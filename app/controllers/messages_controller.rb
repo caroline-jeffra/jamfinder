843d452800f6322109ed4a3fd0fff385730e32f3
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-08-31 17:05:21 +0200

    action cable implemented

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,8 +2,13 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     @message.user = current_user
+    @chatroom = @message.chatroom
     if @message.save
-      redirect_to chats_path
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "message", locals: { message: @message })
+      )
+      head :ok
     else
       render "chats", status: :unprocessable_entity
     end
