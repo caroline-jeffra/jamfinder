10822296e5bbc958ecc2c97fa959c29ee83e18fa
Thiago Souza <thlucioxi@gmail.com>
2023-09-01 14:34:10 +0200

    mapbox-update problem 2. Not fixed

--- app/javascript/controllers/switch_btn_controller.js
+++ app/javascript/controllers/switch_btn_controller.js
@@ -4,7 +4,9 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["switch1", "switch2", "switch3", "switch4"]
   connect() {
-    this.switch2Target.classList.add("mapboxgl-render");
+    console.log("hi")
+    switch2Target.classList.add("mapboxgl-render");
+    console.log("hi2")
   }
   fire1() {
     this.switch1Target.classList.add("mapboxgl-render");
@@ -22,12 +24,5 @@ export default class extends Controller {
     this.switch3Target.classList.remove("btn-outline-primary");
     this.switch4Target.classList.add("btn-outline-primary");
     this.switch4Target.classList.remove("btn-primary");
-  }
-
-  fireRender() {
-    this.switch2Target.classList.add("mapboxgl-render");
-  }
-  setTimeout(){
-
   }
 }
