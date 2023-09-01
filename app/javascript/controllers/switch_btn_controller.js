15288282577676ae22c9894921db04c7f7f6355d
Thiago Souza <thlucioxi@gmail.com>
2023-09-01 13:00:00 +0200

    correct map

--- app/javascript/controllers/switch_btn_controller.js
+++ app/javascript/controllers/switch_btn_controller.js
@@ -4,6 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["switch1", "switch2", "switch3", "switch4"]
   connect() {
+    this.switch2Target.style.visibility = "none";
   }
   fire1() {
     this.switch1Target.classList.add("d-none");
@@ -21,5 +22,12 @@ export default class extends Controller {
     this.switch3Target.classList.remove("btn-outline-primary");
     this.switch4Target.classList.add("btn-outline-primary");
     this.switch4Target.classList.remove("btn-primary");
+  }
+
+  fireRender() {
+    this.switch2Target.classList.add("d-none");
+  }
+  setTimeout(){
+
   }
 }
