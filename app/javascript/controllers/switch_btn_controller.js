1587cd2f21b7477e4167082159554c2ab10629c1
Thiago Souza <thlucioxi@gmail.com>
2023-09-01 15:02:10 +0200

    mapbox-update problem 2. Parcial fixed

--- app/javascript/controllers/switch_btn_controller.js
+++ app/javascript/controllers/switch_btn_controller.js
@@ -4,13 +4,11 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["switch1", "switch2", "switch3", "switch4"]
   connect() {
-    console.log("hi")
-    switch2Target.classList.add("mapboxgl-render");
-    console.log("hi2")
+    this.switch2Target.classList.add("d-none");
   }
   fire1() {
-    this.switch1Target.classList.add("mapboxgl-render");
-    this.switch2Target.classList.remove("mapboxgl-render");
+    this.switch1Target.classList.add("d-none");
+    this.switch2Target.classList.remove("d-none");
     this.switch3Target.classList.remove("btn-primary");
     this.switch3Target.classList.add("btn-outline-primary");
     this.switch4Target.classList.remove("btn-outline-primary");
@@ -18,8 +16,8 @@ export default class extends Controller {
   }
 
   fire2() {
-    this.switch1Target.classList.remove("mapboxgl-render");
-    this.switch2Target.classList.add("mapboxgl-render");
+    this.switch1Target.classList.remove("d-none");
+    this.switch2Target.classList.add("d-none");
     this.switch3Target.classList.add("btn-primary");
     this.switch3Target.classList.remove("btn-outline-primary");
     this.switch4Target.classList.add("btn-outline-primary");
