e860b07607fae0e3433f3655a8acd3e2d54b1293
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-09-04 16:49:39 +0200

    chats

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -3,11 +3,22 @@ class Chatroom < ApplicationRecord
   has_many :user_chatrooms
   before_validation :sort_participant_ids
 
+  validates :participant_ids, presence: true
+
+  # TODO: UNIQUENESS DOES NOT WORK
   validates_uniqueness_of :participant_ids
+
+  def recipient(current)
+    (participants - [current]).first
+  end
+
+  def participants
+    User.find(participant_ids)
+  end
 
   private
 
   def sort_participant_ids
-    self.participant_ids = participant_ids.sort
+    self.participant_ids = participant_ids.uniq.sort
   end
 end
