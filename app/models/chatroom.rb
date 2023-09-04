b38dbcdc39c3ebb6be14935e38fb0e2684e8c4b4
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-09-04 17:29:07 +0200

    validation for minimum participants, existing users

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -1,24 +1,30 @@
 class Chatroom < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :user_chatrooms
-  before_validation :sort_participant_ids
+  before_validation :sort_and_uniquify_participant_ids
 
-  validates :participant_ids, presence: true
+  validates :participant_ids, presence: true, length: { minimum: 2 }
 
-  # TODO: UNIQUENESS DOES NOT WORK
   validates_uniqueness_of :participant_ids
+  validate :users_exist?
 
   def recipient(current)
     (participants - [current]).first
   end
 
   def participants
-    User.find(participant_ids)
+    User.where(id: participant_ids)
   end
 
   private
 
-  def sort_participant_ids
+  def sort_and_uniquify_participant_ids
     self.participant_ids = participant_ids.uniq.sort
+  end
+
+  def users_exist?
+    return true if participant_ids.count == participants.count
+
+    errors.add(:participant_ids, 'Non-existent user(s)')
   end
 end
