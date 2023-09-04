3e65ec4d809a05399c8c437555f5e1022068e922
Caroline Jeffra <caroline.jeffra@gmail.com>
2023-09-04 12:58:41 +0200

    create UserJam from scheduled jam within chat

--- app/controllers/jams_controller.rb
+++ app/controllers/jams_controller.rb
@@ -4,7 +4,18 @@ class JamsController < ApplicationController
     puts jam_params
     if @jam.save
       flash.alert = "Jam booked!"
-      puts "******* jam with users #{jam_params} *******"
+      participants = params[:jam][:participants].split
+      participants.each do |participant|
+        jam_join = UserJam.new
+        jam_join.user = User.find(participant.to_i)
+        jam_join.jam = @jam
+        if jam_join.save
+          puts "#{jam_join} saved"
+        else
+          puts "jam_join not saved :((((((("
+        end
+      end
+      raise
       # TODO: collapse toggleable
     else
       flash.alert = "Please enter a date and time in the future."
