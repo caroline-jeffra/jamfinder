8a5c8d225ee617ea3240011d1b669e0b70c73c1a
Caroline Jeffra <caroline.jeffra@gmail.com>
2023-09-04 16:57:15 +0200

    basic jam card display iteration

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,5 +7,20 @@ class PagesController < ApplicationController
 
   def dashboard
     @user = current_user
+    @jams = current_user.jams
+    @jams = @jams.includes(:users)
+    @months = month_list(@jams)
+  end
+
+  private
+
+  def month_list(scheduled_jams)
+    months = []
+    scheduled_jams.each do |jam|
+      unless months.include? jam.date.mon
+        months << jam.date.mon
+      end
+    end
+    return months
   end
 end
