340b954f5f0ad7bf9ca985e383ffecc5de95a730
Thiago Souza <thlucioxi@gmail.com>
2023-09-05 16:30:21 +0200

    Instrument form

--- app/javascript/controllers/popover_controller.js
+++ app/javascript/controllers/popover_controller.js
@@ -2,12 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="popover"
 export default class extends Controller {
-  static targets = ['bio', 'genre']
+  static targets = ['bio', 'genre', 'instrument']
   bio(){
     this.bioTarget.classList.toggle('d-none')
   }
   genre(){
     this.genreTarget.classList.toggle('d-none')
+  }
+  instrument(){
+    this.instrumentTarget.classList.toggle('d-none')
   }
 
 }
