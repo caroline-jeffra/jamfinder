2e2cb602b3c8071a6f396f968db524e939fc2336
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-09-06 14:31:02 +0200

    break

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -6,4 +6,11 @@ class Instrument < ApplicationRecord
   validates :name, :category, presence: true
   validates_inclusion_of :category, in: CATEGORIES
   validates_uniqueness_of :name
+
+  include PgSearch::Model
+  pg_search_scope :search,
+                  against: :name,
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
