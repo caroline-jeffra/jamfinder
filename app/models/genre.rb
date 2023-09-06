639f8fcbc5ccb11f4cd1f8c2648a0bdd842cdbf9
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-09-06 15:01:03 +0200

    removed changes

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,11 +2,4 @@ class Genre < ApplicationRecord
   has_many :user_genres, dependent: :destroy
   validates :name, presence: true
   validates_uniqueness_of :name
-
-  include PgSearch::Model
-  pg_search_scope :search,
-                  against: :name,
-                  using: {
-                    tsearch: { prefix: true }
-                  }
 end
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -6,11 +6,4 @@ class Instrument < ApplicationRecord
   validates :name, :category, presence: true
   validates_inclusion_of :category, in: CATEGORIES
   validates_uniqueness_of :name
-
-  include PgSearch::Model
-  pg_search_scope :search,
-                  against: :name,
-                  using: {
-                    tsearch: { prefix: true }
-                  }
 end
