929e05355fcab9f19be75fcdc8ce063359375de6
Francesco Zeno Zencovich <francesco_zz_94@hotmail.it>
2023-09-07 13:47:00 +0200

    working

--- app/controllers/jams_controller.rb
+++ app/controllers/jams_controller.rb
@@ -2,25 +2,14 @@ class JamsController < ApplicationController
   def create
     @jam = Jam.new(jam_params)
     puts jam_params
-    participants = params[:jam][:participants].split
-    recipient_id = (participants - [current_user.id.to_s]).first
+    participant_ids = params[:jam][:participants].split
+    participants = [User.find(participant_ids.first.to_i), User.find(participant_ids.last.to_i)]
+    @jam.users = participants
+    recipient_id = (participant_ids - [current_user.id.to_s]).first
     if @jam.save
       flash.alert = "Jam booked!"
-      participants.each do |participant|
-        jam_join = UserJam.new
-        jam_join.user = User.find(participant.to_i)
-        jam_join.jam = @jam
-        if jam_join.save
-          flash.alert = "Jam saved to your profile"
-        else
-          flash.alert = "There was an error saving your jam to your profile. Please try again."
-        end
-        redirect_to chats_path(recipient: recipient_id)
-        return
-      end
-    else
-      # TODO: show form errors
-      flash.alert = "Please enter a date and time in the future."
+      redirect_to chats_path(recipient: recipient_id)
+      return
     end
   end
 
